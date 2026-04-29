a3c5530c0e0677c30662df5be63cd232ba4a65eb
Farouq Abou Umar <info@fitrahmedia.nl>
2026-04-29 17:00:33 +0100

    [FEAT]: [REPORT-01]: Add ContentReport Firestore model + ReportTargetType/Reason/Status enums

---
+++ backend/src/main/java/com/albunyaan/tube/model/ReportReason.java
@@ -0,0 +1,15 @@
+package com.albunyaan.tube.model;
+
+public enum ReportReason {
+    MUSIC,
+    NUDITY,
+    BAD_LANGUAGE,
+    FLIRTING,
+    ROMANCE,
+    AWRAH,
+    SHIRK,
+    BIDAH,
+    VIOLENCE,
+    MISINFORMATION,
+    OTHER
+}
---
+++ backend/src/main/java/com/albunyaan/tube/model/ReportStatus.java
@@ -0,0 +1,5 @@
+package com.albunyaan.tube.model;
+
+public enum ReportStatus {
+    PENDING, RESOLVED, REJECTED
+}
